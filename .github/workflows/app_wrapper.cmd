dcb396f2860ea28267749b91cfc8159f433cc4b0
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2026-03-17 17:08:15 -0700

    Add logs
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

--- .github/workflows/app_wrapper.cmd
+++ .github/workflows/app_wrapper.cmd
@@ -4,10 +4,13 @@ setlocal
 set PSEXEC=%ProgramData%\chocolatey\bin\PsExec.exe
 if not exist "%PSEXEC%" (
   echo ERROR: PsExec.exe not found in Chocolatey bin.
+  dir "%ProgramData%\chocolatey\bin%"
   exit /b 1
 )
 
-"%PSEXEC%" -accepteula -l -w "%CD%" cmd.exe /v:on /c "bazelisk run :app"
+echo Running Vulkan smoke test via %PSEXEC%
+"%PSEXEC%" -nobanner -accepteula -l -w "%CD%" cmd.exe /v:on /c "bazelisk run :app"
 set EXIT_CODE=%ERRORLEVEL%
+echo Vulkan smoke test exit code %EXIT_CODE%
 
 exit /b %EXIT_CODE%
